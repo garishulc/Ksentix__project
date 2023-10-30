b742a010810ee65800277fd287009ac8a8dde094
garishulc <33732793+garishulc@users.noreply.github.com>
2023-10-30 18:39:31 +0300

    Delete Ksentix__project directory

--- Ksentix__project/README.md
+++
@@ -1,3 +0,0 @@
-# Ksentix__project
-Пример размещенного сайта
-[F&L_consalding](https://garishulc.github.io/Ksentix__project/)
--- Ksentix__project/src/js/index.js
+++
@@ -1,6 +0,0 @@
-// подключаем необходимые блоки блоки: типа about,
-// contacts, modules
-
-import mobileNav from './modules/mobile-nav.js';
-mobileNav();
-const $ = require("jquery");
